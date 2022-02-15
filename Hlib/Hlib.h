a9df2e755bde98f35e1e1bb2ea205f2b51535a99
errantes <44671333+excalibur-knob@users.noreply.github.com>
2022-02-15 21:45:15 +0400

    Update Hlib.h

--- Hlib/Hlib.h
+++ Hlib/Hlib.h
@@ -12,11 +12,14 @@
 #include <iphlpapi.h>
 #include <WS2tcpip.h>
 #include <windows.h>
-#include <thread>
+#include <vector>
+#include <unordered_map>
 
 #pragma comment(lib, "Ws2_32.lib")
 
-namespace Hlib{
+#define EMPTY ""
+
+namespace Hlib {
 
     enum IPv {
         IPv4,
@@ -32,28 +35,64 @@ namespace Hlib{
         TRACE,
     };
 
-    class Sock {
+    void req();
+    std::string res(std::string resdata, std::string status = "200 OK", std::string ct = "Content-Type: text/html");
+
+    class HTTP {
 
     private:
-        WSADATA wsaData;
         struct sockaddr_in address;
-        const char*   s_ipAddress;
+        SOCKET s_socket;
+        SOCKET c_socket;
+        WSADATA wsaData;
+        std::string mapp;
+        std::string str;
+        std::string method;
+        std::string router;
+        std::string data;
+        std::string httpVer;
+        std::string ct;
+        std::string cl;
+        std::string hdr;
+
+        int dataindex;
+        int recvHTTP();
+        int sendHTTP();
         int __ip;
         int _port;
         int _result;
-        SOCKET s_socket;
-        SOCKET c_socket;
-        int c_result;
-        char recvbuf[512];
-        int recvbuflen = 512;
-        std::string local = "127.0.0.1";
         int c_send;
+        int recvbuflen = 512;
+        int c_recv;
         int new_socket;
-        char hello[100] = "Hello from server";
+
+        bool run;
+        bool fav;
+
+        void recvHTTP(int sock, char *buff, size_t len, int flag = 0);
+        void parseData(std::string buff);
+        void sendHTTP(int sock, char *buff, size_t len, int flag = 0);
+
+        const char* s_ipAddress;
+        const char *bdata;
+        char *buffHT;
+        char recvbuf[512];
+        char *local = "127.0.0.1";
+        char *HTTPdata="HTTP/1.1 , std::string stutus\nContent-Type: text/html\nContent-Length: 30\n\n<h1> Hello world! </h1>";
+        char buffer[30000] = {0};
+
+        std::unordered_map<std::string, std::vector<std::string>> routers;
+        std::unordered_map<std::string, std::vector<std::string>> resMap;
     public:
-        Sock(IPv _ipv, int service, int protocol, int port);
-        int disconnect;
-        void inputU();
+        HTTP();
+        int createServer(IPv _ipv, int type, int protocol, int port);
+        void ShutDown(int c_sock, int s_sock, int exit_code = SD_SEND);
+        void Listen();
+        void Checker(std::string method);
+        void Get(std::string path, std::string res);
+        void Post(std::string path, std::string res);
+        void Put(std::string path, std::string res);
+        void _Delete(std::string path, void callBack());
     };
 }
 
